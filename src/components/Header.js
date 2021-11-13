4cc7d38186097b986aabb6b6e420601ebd458818
Nic Scobey <nicscobey@gmail.com>
2021-11-13 14:21:52 -0800

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header(props){
       <div className="nav">
         <Link to="/">Index</Link>
         <Link to="/:id">Show</Link>
+        <h3>Here's an update!</h3>
       </div>
     )
   } 
